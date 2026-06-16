3f752a446b6ce702062fb8eb205565ffe88199a2
Shiso <165101027+Watson912@users.noreply.github.com>
2026-06-16 16:39:04 -0700

    Community tab and some additional fixed (#82)

--- public/scripts/firebase-config.js
+++ public/scripts/firebase-config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js';
 import { getAuth } from 'https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.14.1/firebase-firestore.js';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyDySk5vVrsx3WsExD5RwJSxY96TzQl9zak',
@@ -12,3 +13,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
